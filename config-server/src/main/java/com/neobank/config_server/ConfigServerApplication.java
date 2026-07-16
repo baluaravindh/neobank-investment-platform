35c5b9d32593fd10b5735de012b274029e2a67f1
dev global <balu.gounipuram@gmail.com>
2026-07-16 12:32:26 +0530

    Initial Project Structure

---
+++ config-server/src/main/java/com/neobank/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.neobank.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
